435ff81b47e6b825e6cb730ec3bfa332e4a2902b
BobRocky <kiryxa02iks@gmail.com>
2018-04-27 20:40:13 +0300

    ф

--- Client,Server/message.go
+++ Client,Server/message.go
@@ -8,3 +8,29 @@ type Message struct {
 func (self *Message) String() string {
 	return self.Author + " говорит: " + self.Body
 }
+
+/*
+func shifr(conn net.Conn) string {
+	//ключ
+	password := []byte("some")
+
+	defer conn.Close()
+	//Принятие слова
+	input := make([]byte, (1024))
+	n, err := conn.Read(input)
+	if n == 0 || err != nil {
+		fmt.Println("Read error:", err)
+		os.Exit(1)
+	}
+
+	//Расшифруем
+	original, decrypt_error := lithcrypt.Decrypt(password, input[0:n])
+	if decrypt_error != nil {
+		fmt.Println("Failed to decrypt:", decrypt_error)
+		os.Exit(1)
+	}
+
+	source := string(original)
+	//Слово в source
+	return source
+}
