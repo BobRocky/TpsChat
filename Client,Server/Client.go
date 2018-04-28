1abcf418f2613904f21d505249f99b1ab746d04a
SophiaSmit <sophiasmit59@gmail.com>
2018-04-28 16:40:25 +0300

    Клиент, мусор

--- Client,Server/Client.go
+++ Client,Server/Client.go
@@ -10,12 +10,36 @@ import (
 )
 
 var source string
-var n int
-var input []byte
-var encrypted []byte
-var proverka string
-var otvet string
 
+//Из введеного достает команду
+func comand() string {
+	arr := []byte(source) //из переменной q делаем массив arr
+	y := arr[0:2]         //из arr вырезаем символы от 0 до 2 в переменную y
+	return string(y)      //Функцие proverka даем значение y в расширении string
+}
+
+//Из введеного достает id
+func id() string {
+	arr := []byte(source) //из переменной q делаем массив arr
+	y := arr[3:4]         //переменной даем символы от arr от 3 до 4 символа
+	return string(y)      //Функцие proverka даем значение y в расширении string
+}
+
+//Из введеного достает текст
+func text() string {
+	arr := []byte(source) //из переменной q делаем массив arr
+	x := arr[5:len(arr)]  //переменной даем символы от arr от 5 символа до длины arr
+	return string(x)      //Функцие proverka2 даем значение y в расширении string
+}
+
+//Функция... Эмм... Пробела?....
+func probel() string {
+	arr := []byte(source) //из переменной q делаем массив arr
+	y := arr[2:3]         //переменной даем символы от arr от 2 до 3 символа
+	return string(y)      //Функцие proverka даем значение y в расширении string
+}
+
+//Функция ввода слова
 func Scan1() string {
 	in := bufio.NewScanner(os.Stdin)
 	in.Scan()
@@ -25,99 +49,176 @@ func Scan1() string {
 	return in.Text()
 }
 
-func shifr(conn net.Conn) string {
-	//ключ
+//Функция шифра
+func shir() string {
 	password := []byte("some")
+	payload := []byte(source)
+	encrypted, encrypt_error := lithcrypt.Encrypt(password, payload)
+	if encrypt_error != nil {
+		fmt.Println("Failed to encrypt:", encrypt_error)
+		os.Exit(1)
+	}
+	return string(encrypted)
+}
 
-	defer conn.Close()
-	//Принятие слова
+//Функция получения сообщения и расшифровка
+func rshifr(conn net.Conn) string {
+	password := []byte("some")
+	//defer conn.Close()
+
 	input := make([]byte, (1024))
 	n, err := conn.Read(input)
 	if n == 0 || err != nil {
-		fmt.Println("Read error:", err)
+		fmt.Println("Read error123:", err)
 		os.Exit(1)
 	}
-
-	//Расшифруем
 	original, decrypt_error := lithcrypt.Decrypt(password, input[0:n])
 	if decrypt_error != nil {
 		fmt.Println("Failed to decrypt:", decrypt_error)
 		os.Exit(1)
 	}
-
-	source := string(original)
-	//Слово в source
-	return source
+	return string(original)
 }
 
 func main() {
-
-	password := []byte("some")
-
-	conn, err := net.Dial("tcp", "localhost:4545")
+	//Идет подключение к серверу
+	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
 
-	proverka := shifr(conn)
-	if proverka == "errl" { //errl Ошибка логина
-		fmt.Println("Логин введен не верно\nПовторите попытку: ")
-	}
-
 	for {
-		fmt.Print("Password: ")
+		//Ввод логина
+		fmt.Print("login: ")
 		source = Scan1()
-
-		//************************Шифруем********************************************
-		payload := []byte(source)
-		encrypted, encrypt_error := lithcrypt.Encrypt(password, payload)
-		if encrypt_error != nil {
-			fmt.Println("Failed to encrypt:", encrypt_error)
-			os.Exit(1)
-		}
-		//***************************************************************************
-
-		//******************Отправка сообщения серверу******************
-		if n, err := conn.Write(encrypted); n == 0 || err != nil {
+		shir := shir()
+		//Отправляем серверу наш логин
+		if n, err := conn.Write([]byte(shir)); n == 0 || err != nil {
 			fmt.Println(err)
 			return
 		}
-
-		proverka := shifr(conn)
+		//Проверка на правильность логина
+		proverka := rshifr(conn)
+		if proverka == "errl" { //errl Ошибка логина
+			fmt.Println("Логин введен не верно\nПовторите попытку: ")
+		} else {
+			break
+		}
+	}
+	for {
+		//Ввод пароля
+		fmt.Print("Password: ")
+		source = Scan1()
+		shir := shir()
+		if n, err := conn.Write([]byte(shir)); n == 0 || err != nil {
+			fmt.Println(err)
+			return
+		}
+		proverka := rshifr(conn)
+		//Проверка пароля
 		if proverka == "errp" { //errl Ошибка логина
 			fmt.Println("Пароль введен не верно\nПовторите попытку: ")
 		} else {
 			break
 		}
+	}
 
-		for {
-			source = Scan1()
+	for {
+		fmt.Println("Вы успешно автоватезировались!")
+		fmt.Println("Чтобы узнать список команд напишите /help")
+		//Ввод слова с клавиатуры
+		source = Scan1()
+		//Сразу приравниваем функции к переменным
+		comand := comand()
+		id := id()
+		text := text()
+		probel := probel()
 
-			if source == "/help" {
-				fmt.Println("/l - список челиков онлайн\n/w id написать челику")
-			}
-			//******************Шифруем**********************************************
-			payload := []byte(source)
-			encrypted, encrypt_error := lithcrypt.Encrypt(password, payload)
-			if encrypt_error != nil {
-				fmt.Println("Failed to encrypt:", encrypt_error)
-				os.Exit(1)
-			}
-			//***************************************************************************
-
-			//******************Отправка сообщения серверу******************
+		if comand == "/h" {
+			fmt.Println("\t\tСписок команд:\n/list-Посмотреть список клиентов онлайн\n/write [id] [текст] - напиать пользователю\n/back - Выйти из диолога с собеседником\n/exit - Выход из программы.")
+		}
+		if comand == "/l" {
+			source = comand
+			encrypted := []byte(shir())
 			if n, err := conn.Write(encrypted); n == 0 || err != nil {
 				fmt.Println(err)
 				return
 			}
-
-			//*****************************************************************
-			fmt.Println("Что-то говорит тебе ")
-			fmt.Println(otvet)
-
 		}
 
+		otvet := rshifr(conn)
+		fmt.Println(otvet)
+		if comand == "/w" {
+			switch {
+			case id == "1":
+				for text == "/back" {
+					fmt.Println("Вы вошли в диолог с пользователем ", id)
+					source = Scan1()
+					message := comand + probel + id + probel + text
+					source = message
+					encrypted := []byte(shir())
+					if n, err := conn.Write(encrypted); n == 0 || err != nil {
+						fmt.Println(err)
+						return
+					}
+				}
+			case id == "2":
+				for text == "/back" {
+					fmt.Println("Вы вошли в диолог с пользователем ", id)
+					source = Scan1()
+					message := comand + probel + id + probel + text
+					source = message
+					encrypted := []byte(shir())
+					if n, err := conn.Write(encrypted); n == 0 || err != nil {
+						fmt.Println(err)
+						return
+					}
+				}
+			case id == "3":
+				for text == "/back" {
+					fmt.Println("Вы вошли в диолог с пользователем ", id)
+					source = Scan1()
+					message := comand + probel + id + probel + text
+					source = message
+					encrypted := []byte(shir())
+					if n, err := conn.Write(encrypted); n == 0 || err != nil {
+						fmt.Println(err)
+						return
+					}
+				}
+			case id == "4":
+				for text == "/back" {
+					fmt.Println("Вы вошли в диолог с пользователем ", id)
+					source = Scan1()
+					message := comand + probel + id + probel + text
+					source = message
+					encrypted := []byte(shir())
+					if n, err := conn.Write(encrypted); n == 0 || err != nil {
+						fmt.Println(err)
+						return
+					}
+				}
+			case id == "5":
+				for text == "/back" {
+					fmt.Println("Вы вошли в диолог с пользователем ", id)
+					source = Scan1()
+					message := comand + probel + id + probel + text
+					source = message
+					encrypted := []byte(shir())
+					if n, err := conn.Write(encrypted); n == 0 || err != nil {
+						fmt.Println(err)
+						return
+					}
+				}
+			}
+		}
+
+		if comand == "/e" {
+			os.Exit(1)
+		} else {
+			fmt.Println("Введите сначало команду\nДля просмотра команд введите /help")
+		}
 	}
 }
--- Мусор.go
+++ Мусор.go
@@ -1,10 +1,45 @@
-/*package main
+package main
 
 import (
 	"bufio"
 	"fmt"
+	"net"
 	"os"
+
+	"github.com/richard-lyman/lithcrypt"
 )
+
+var source string
+
+//Из введеного достает команду
+func comand() string {
+	arr := []byte(source) //из переменной q делаем массив arr
+	y := arr[0:2]         //из arr вырезаем символы от 0 до 2 в переменную y
+	return string(y)      //Функцие proverka даем значение y в расширении string
+}
+
+//Из введеного достает id
+func id() string {
+	arr := []byte(source) //из переменной q делаем массив arr
+	y := arr[3:4]         //переменной даем символы от arr от 3 до 4 символа
+	return string(y)      //Функцие proverka даем значение y в расширении string
+}
+
+//Из введеного достает текст
+func text() string {
+	arr := []byte(source) //из переменной q делаем массив arr
+	x := arr[5:len(arr)]  //переменной даем символы от arr от 5 символа до длины arr
+	return string(x)      //Функцие proverka2 даем значение y в расширении string
+}
+
+//Функция... Эмм... Пробела?....
+func probel() string {
+	arr := []byte(source) //из переменной q делаем массив arr
+	y := arr[2:3]         //переменной даем символы от arr от 2 до 3 символа
+	return string(y)      //Функцие proverka даем значение y в расширении string
+}
+
+//Функция ввода слова
 func Scan1() string {
 	in := bufio.NewScanner(os.Stdin)
 	in.Scan()
@@ -13,6 +48,198 @@ func Scan1() string {
 	}
 	return in.Text()
 }
+
+//Функция шифра
+func shir() string {
+	password := []byte("some")
+	payload := []byte(source)
+	encrypted, encrypt_error := lithcrypt.Encrypt(password, payload)
+	if encrypt_error != nil {
+		fmt.Println("Failed to encrypt:", encrypt_error)
+		os.Exit(1)
+	}
+	return string(encrypted)
+}
+
+//Функция получения сообщения и расшифровка
+func rshifr(conn net.Conn) string {
+	password := []byte("some")
+	//defer conn.Close()
+
+	input := make([]byte, (1024))
+	n, err := conn.Read(input)
+	if n == 0 || err != nil {
+		fmt.Println("Read error123:", err)
+		os.Exit(1)
+	}
+	original, decrypt_error := lithcrypt.Decrypt(password, input[0:n])
+	if decrypt_error != nil {
+		fmt.Println("Failed to decrypt:", decrypt_error)
+		os.Exit(1)
+	}
+	return string(original)
+}
+
+func main() {
+	//Идет подключение к серверу
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		//Ввод логина
+		fmt.Print("login: ")
+		source = Scan1()
+		shir := shir()
+		//Отправляем серверу наш логин
+		if n, err := conn.Write([]byte(shir)); n == 0 || err != nil {
+			fmt.Println(err)
+			return
+		}
+		//Проверка на правильность логина
+		proverka := rshifr(conn)
+		if proverka == "errl" { //errl Ошибка логина
+			fmt.Println("Логин введен не верно\nПовторите попытку: ")
+		} else {
+			break
+		}
+	}
+	for {
+		//Ввод пароля
+		fmt.Print("Password: ")
+		source = Scan1()
+		shir := shir()
+		if n, err := conn.Write([]byte(shir)); n == 0 || err != nil {
+			fmt.Println(err)
+			return
+		}
+		proverka := rshifr(conn)
+		//Проверка пароля
+		if proverka == "errp" { //errl Ошибка логина
+			fmt.Println("Пароль введен не верно\nПовторите попытку: ")
+		} else {
+			break
+		}
+	}
+
+	for {
+		fmt.Println("Вы успешно автоватезировались!")
+		fmt.Println("Чтобы узнать список команд напишите /help")
+		//Ввод слова с клавиатуры
+		source = Scan1()
+		//Сразу приравниваем функции к переменным
+		comand := comand()
+		id := id()
+		text := text()
+		probel := probel()
+
+		if comand == "/h" {
+			fmt.Println("\t\tСписок команд:\n/list-Посмотреть список клиентов онлайн\n/write [id] [текст] - напиать пользователю\n/back - Выйти из диолога с собеседником\n/exit - Выход из программы.")
+		}
+		if comand == "/l" {
+			source = comand
+			encrypted := []byte(shir())
+			if n, err := conn.Write(encrypted); n == 0 || err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+
+		otvet := rshifr(conn)
+		fmt.Println(otvet)
+		if comand == "/w" {
+			switch {
+			case id == "1":
+				for text == "/back" {
+					fmt.Println("Вы вошли в диолог с пользователем ", id)
+					source = Scan1()
+					message := comand + probel + id + probel + text
+					source = message
+					encrypted := []byte(shir())
+					if n, err := conn.Write(encrypted); n == 0 || err != nil {
+						fmt.Println(err)
+						return
+					}
+				}
+			case id == "2":
+				for text == "/back" {
+					fmt.Println("Вы вошли в диолог с пользователем ", id)
+					source = Scan1()
+					message := comand + probel + id + probel + text
+					source = message
+					encrypted := []byte(shir())
+					if n, err := conn.Write(encrypted); n == 0 || err != nil {
+						fmt.Println(err)
+						return
+					}
+				}
+			case id == "3":
+				for text == "/back" {
+					fmt.Println("Вы вошли в диолог с пользователем ", id)
+					source = Scan1()
+					message := comand + probel + id + probel + text
+					source = message
+					encrypted := []byte(shir())
+					if n, err := conn.Write(encrypted); n == 0 || err != nil {
+						fmt.Println(err)
+						return
+					}
+				}
+			case id == "4":
+				for text == "/back" {
+					fmt.Println("Вы вошли в диолог с пользователем ", id)
+					source = Scan1()
+					message := comand + probel + id + probel + text
+					source = message
+					encrypted := []byte(shir())
+					if n, err := conn.Write(encrypted); n == 0 || err != nil {
+						fmt.Println(err)
+						return
+					}
+				}
+			case id == "5":
+				for text == "/back" {
+					fmt.Println("Вы вошли в диолог с пользователем ", id)
+					source = Scan1()
+					message := comand + probel + id + probel + text
+					source = message
+					encrypted := []byte(shir())
+					if n, err := conn.Write(encrypted); n == 0 || err != nil {
+						fmt.Println(err)
+						return
+					}
+				}
+			}
+		}
+
+		if comand == "/e" {
+			os.Exit(1)
+		} else {
+			fmt.Println("Введите сначало команду\nДля просмотра команд введите /help")
+		}
+	}
+}
+
+/*package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Scan1() string {
+	in := bufio.NewScanner(os.Stdin)
+	in.Scan()
+	if err := in.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Ошибка ввода:", err)
+	}
+	return in.Text()
+}
+
 var q = Scan1() //Объявляем переменную вне функции
 
 func proverka() string {
@@ -31,22 +258,68 @@ func proverka3() string {
 	y := arr[3:4]    //переменной даем символы от arr от 3 до 4 символа
 	return string(y) //Функцие proverka даем значение y в расширении string
 }
+func proverka4() string {
+	arr := []byte(q) //из переменной q делаем массив arr
+	y := arr[2:3]    //переменной даем символы от arr от 3 до 4 символа
+	return string(y) //Функцие proverka даем значение y в расширении string
+}
 
 func main() {
-	var comand string
-	var slovo string
-	var id string
-	comand = proverka()
-	if comand == "/w" {
-		id = proverka3()
-		slovo = proverka2()
-		fmt.Print("Пользвателю ", id, ": ", slovo)
-	}
-
-}*/
+	comand := proverka()
+	id := proverka3()
+	text := proverka2()
+	probel := proverka4()
+	fmt.Println(comand)
+	fmt.Println(id)
+	fmt.Println(text)
+	privet := comand + probel + id + probel + text
+	fmt.Print(privet)
+}
 
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
 
+	"github.com/richard-lyman/lithcrypt"
+)
+
+var source string
+
+func Scan1() string {
+	in := bufio.NewScanner(os.Stdin)
+	in.Scan()
+	if err := in.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Ошибка ввода:", err)
+	}
+	return in.Text()
 }
+
+func shir() string {
+	password := []byte("some")
+	payload := []byte(source)
+	encrypted, encrypt_error := lithcrypt.Encrypt(password, payload)
+	if encrypt_error != nil {
+		fmt.Println("Failed to encrypt:", encrypt_error)
+		os.Exit(1)
+	}
+	return string(encrypted)
+}
+
+func main() {
+	source = Scan1()
+	q := shir()
+	password := []byte("some")
+	original, decrypt_error := lithcrypt.Decrypt(password, []byte(q))
+	if decrypt_error != nil {
+		fmt.Println("Failed to decrypt:", decrypt_error)
+		os.Exit(1)
+	}
+
+	fmt.Println(q)
+	fmt.Println(original)
+	fmt.Println(string(original))
+}
+*/
